e2711f27137659dbcd7b37ea0f08586f2b65f2e4
barabulko <barabulko@yahoo.com>
2023-04-11 22:49:14 +0500

    first commit

---
+++ task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("task41");
+
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 !!!ошибка в примере к задаче!!!*/
+
+int[] arrayInput(){
+    Console.Write("enter size of array = ");
+    int M = int.Parse(Console.ReadLine());
+    int[] array = new int[M]; 
+    for (int i=0; i<M; i++){
+        Console.Write($"enter element #{i} = ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+int countPositives(int[] array){
+    int result = 0;
+    for (int i=0; i<array.Length; i++){
+        if (array[i]>0) result++;
+    }
+    return result;
+}
+
+void test(){
+    Console.WriteLine(countPositives(arrayInput()));
+}
+
+test();
---
+++ task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("task43");
+
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+double userInput(string name){
+    Console.Write($"enter {name} = ");
+    return double.Parse(Console.ReadLine());
+}
+
+void main(){
+    double k1 = userInput("k1");
+    double b1 = userInput("b1");
+    double k2 = userInput("k2");
+    double b2 = userInput("b2");
+
+    if (k1==k2) {
+        Console.WriteLine("Прямые параллельны");
+        return;
+    }
+
+    double x = (b2-b1)/(k1-k2);
+    double y = k1*x+b1;
+
+    Console.WriteLine($"{k1}x+{b1} пересекает {k2}x+{b2} в точке ({x}, {y})");
+    return;
+}
+
+main();
